0ab76f0cdb34b048cd7add8016fa67096f32689c
alvin <alvinec09@yahoo.com>
2015-10-19 20:45:24 -0700

    add home view and configure routes to include authenitcation of twitter. Add twitter and omniauth gem

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,4 +1,9 @@
 class TwitterController < ApplicationController
   def authentication
+  	redirect_to "/auth/twitter"
+  end
+
+  #remember to check if user is login before enter the home page
+  def home
   end
 end
