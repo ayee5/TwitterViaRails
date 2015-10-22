c9d59463e70c42a1c5934bcf7411f8abf65a34f8
alvin <alvinec09@yahoo.com>
2015-10-21 21:32:37 -0700

    Write tweet function and increase the size of the tweet textarea

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,14 +1,27 @@
 class TwitterController < ApplicationController
+
   def authentication
   	redirect_to "/auth/twitter"
   end
 
-  #remember to check if user is login before enter the home page
   def home
-  	#get parameter that should be used to post to twitter
-	textarea = params[:textarea]
-	#use line below to render out results for debuggin
-	#render :text => textarea
+    #use tweet get parameter and post to twitter
+    message = params[:tweet]
+    tweetMessage(message)
   end
 
+  #post a message to twitter
+  def tweetMessage(message)
+    if(session[:user_id] && message != nil && message !="")
+      current_user.twitter.update(message);
+    end
+  end
 end
+
+
+
+#use line below to render out results for debuggin
+#render :text => textarea
+
+#use line below to render a statement to the rails console
+#Rails.logger.warn "sharad verma is logging"
